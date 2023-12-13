f5f719d9ce39206d10999fbceb20f4148cd74b1c
franklinoli <f.oliverosafonso2@gmail.com>
2023-12-13 09:39:56 +0100

    ejemplo

---
+++ SegundaEva/src/main/java/EjemploModular/Calculadora.java
@@ -0,0 +1,20 @@
+package EjemploModular;
+
+public class Calculadora {
+    public static int suma (int x, int y) {
+        int resultado = x + y;
+        return resultado;
+    }
+    public static int resta (int x,int y) {
+        int resultado2 = x - y;
+        return resultado2;
+    }
+    public static int multi (int x,int y) {
+        int resultado3 = x * y;
+        return resultado3;
+    }
+    public static int divi (int x,int y) {
+        int resultado4 = x / y;
+        return resultado4;
+    }
+}
