b1051b8e8e23f016ea68e3ff653af8d6c494cad5
franklinoli <f.oliverosafonso2@gmail.com>
2023-12-18 14:29:52 +0100

    .

--- Granja/src/main/java/Gallina.java
+++ Granja/src/main/java/Gallina.java
@@ -1,2 +1,35 @@
 public class Gallina {
+    private String nombre;
+    private int edad;
+    private float peso;
+
+
+    private static final String[] nombres ={"Paco","Max","Quien","Goku","MartiMcfly"};
+    public static String getNombreAleatorio() {
+        return nombres[(int) (Math.random()*5)];
+    }
+
+    public Gallina(String nombre, int edad, float peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+
+    }
+    public Gallina() {
+        nombre = getNombreAleatorio();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Me llamo " + nombre + " peso " + peso + "KG" + " y tengo " + edad + " años");
+    }
+
+
 }
--- Granja/src/main/java/Main.java
+++ Granja/src/main/java/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        String name = "Turu";
+        float peso = 5;
+        int edad = 2;
+        Gallina gallina2 = new Gallina(name,edad,peso);
+        gallina2.mostrarInfo();
+
+    }
 }
