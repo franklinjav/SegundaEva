0ae5646fed72710eee0d7a3e44d07af4f00bfd3d
franklinoli <f.oliverosafonso2@gmail.com>
2023-12-20 11:43:16 +0100

    fknk

--- Granja/src/main/java/Main.java
+++ Granja/src/main/java/Main.java
@@ -1,11 +1,58 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        String name = "Turu";
-        float peso = 5;
-        int edad = 2;
-        Gallina gallina2 = new Gallina(name,edad,peso);
-        gallina2.mostrarInfo();
-        System.out.println("nada");
-        System.out.println("nada 2.0");
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+        String opcion = "10";
+        while (!opcion.equals("5")) {
+            System.out.println("Introduzca:\n 1 Para acceder a la gallina\n 2 Para acceder a las ovejas\n 3 Para acceder a los cerdos\n 4 Para acceder a las vacas\n 5 Para salir");
+            opcion = teclado.nextLine();
+            switch (opcion) {
+                case "1":
+                    break;
+                case "2":
+                    int opcion_oveja = 10;
+                    while (opcion_oveja != 3) {
+                        System.out.println("Introduzca:\n 1 Si desea generar datos aleatorios de una oveja\n 2 Si desea introducir los dato de forma manual\n 3 Si desea salir");
+                        opcion_oveja = teclado.nextInt();
+                        teclado.nextLine();
+                        switch (opcion_oveja) {
+                            case 1:
+                                System.out.println("---------------------------------------------");
+                                Oveja ovejarandom = new Oveja();
+                                System.out.println(ovejarandom.toString());
+                                System.out.println("---------------------------------------------");
+                                break;
+                            case 2:
+                                System.out.println("Introduzca el nombre de la oveja");
+                                String nombre = teclado.nextLine();
+                                System.out.println("Introduzca el nombre de la oveja");
+                                float peso = teclado.nextFloat();
+                                System.out.println("Introduzca el nombre de la oveja");
+                                int edad = teclado.nextInt();
+                                System.out.println("Introduzca el nombre de la oveja");
+                                float lana = teclado.nextFloat();
+                                break;
+                            case 3:
+                                System.out.println("Saliendo...");
+                                break;
+                            default:
+                                System.out.println("No ha introducido una opción válida");
+                                break;
+                        }
+                        break;
+                    }
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                default:
+                    System.out.println("No ha introducido una opción válida");
+                    break;
+            }
+        }
     }
 }
