907bf4f3080ea2a3e1bceafa9a640560be7fb55a
franklinjav <franklin.oliveros@educa.madrid.org>
2024-01-26 12:31:16 +0100

    26/01/2024

--- SegundaEva/src/main/java/EjeMedicos/Main.java
+++ SegundaEva/src/main/java/EjeMedicos/Main.java
@@ -1,4 +1,7 @@
 package src.main.java.EjeMedicos;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- SegundaEva/src/main/java/EjeMedicos/MedicoAsistenciaDomiciliaria.java
+++ SegundaEva/src/main/java/EjeMedicos/MedicoAsistenciaDomiciliaria.java
@@ -1,13 +1,45 @@
 package src.main.java.EjeMedicos;
 
+import java.util.Arrays;
+
 public class MedicoAsistenciaDomiciliaria extends Medico{
     //Atributos
 
     private int[] km_visita;
+    private double tarifakm;
+
+    //Constructores
+
+
+    public MedicoAsistenciaDomiciliaria() {
+        km_visita = new int[31];
+        for (int i = 0; i < km_visita.length; i++) {
+            km_visita[i] = (int) (Math.random()*50+1);
+        }
+        tarifakm = 2;
+    }
+
+    public MedicoAsistenciaDomiciliaria(String nombre, int edad, String sexo, double horas, double salario_hora, int[] km_visita, double tarifakm) {
+        super(nombre, edad, sexo, horas, salario_hora);
+        this.km_visita = km_visita;
+        this.tarifakm = tarifakm;
+    }
+
+    //Métodos
+    @Override
+    public double calcularSalario() {
+        int sum = 0;
+        for (int i = 0; i < km_visita.length; i++) {
+            sum = sum + km_visita[i];
+        }
+        return sum*tarifakm;
+    }
+
+    //toSting
 
 
     @Override
-    public double calcularSalario() {
-        return 0;
+    public String toString() {
+        return super.toString() + String.format("Los kilometros recorridos cada día del mes son:\n" + Arrays.toString(km_visita));
     }
 }
