4ae18ac5db54dd3083c5e2c64be713679cea1e36
franklinjav <franklin.oliveros@educa.madrid.org>
2024-02-05 13:38:28 +0100

    05/02/2024 Ajedrez

--- Ajedrez/src/main/java/ClasesAjedrez/Movimiento.java
+++ Ajedrez/src/main/java/ClasesAjedrez/Movimiento.java
@@ -8,10 +8,18 @@ public class Movimiento {
 
     //Constructores
 
-
+    /**
+     * Constructor vacío de Movimiento
+     */
     public Movimiento() {
     }
 
+    /**
+     * Constructor de la clase Moviento que recibe dos argumentos de entrada (ambos de tipo posición) que serán la posición
+     * inicial (posIni) y la posición final (posFin)
+     * @param posIni Posición inicial
+     * @param posFin Posición final
+     */
     public Movimiento(Posicion posIni, Posicion posFin) {
         this.posIni = posIni;
         this.posFin = posFin;
@@ -19,26 +27,45 @@ public class Movimiento {
 
     //Gets y Sets
 
-
+    /**
+     * Método get para obtener el valor de la posición inicial (posIni)
+     * @return posIni
+     */
     public Posicion getPosIni() {
         return posIni;
     }
 
+    /**
+     * Método para cambiar el valor de la posición inicial (posIni)
+     * @param posIni Posición inicial
+     */
     public void setPosIni(Posicion posIni) {
         this.posIni = posIni;
     }
 
+    /**
+     * Método get para obtener el valor de la posición final (posFin)
+     * @return posFin
+     */
     public Posicion getPosFin() {
         return posFin;
     }
 
+    /**
+     * Método para cambiar el valor de la posición final (posFin)
+     * @param posFin Posición final
+     */
     public void setPosFin(Posicion posFin) {
         this.posFin = posFin;
     }
 
     //toString
 
-
+    /**
+     * Método toString generado automáticamente para imprimir los datos de los
+     * atributos de posición inicial (posIni) y posición final (posFin)
+     * @return toString
+     */
     @Override
     public String toString() {
         return "Movimiento{" +
@@ -49,6 +76,11 @@ public class Movimiento {
 
     //Métodos
 
+    /**
+     * Método esVertical que no recibe parámetros de entrada y nos devuelve un booleano (vertical) que nos indica
+     * en caso de ser verdadero que el movimiento es vertical
+     * @return vetical
+     */
     public boolean esVertical() {
         boolean vertical = false;
         if (posIni.getColumna() == posFin.getColumna() && posIni.getFila() != posFin.getFila()) {
@@ -56,6 +88,12 @@ public class Movimiento {
         }
         return vertical;
     }
+
+    /**
+     * Método esHorizontal que no recibe parámetros de entrada y nos devuelve un booleano (horizontal) que nos indica
+     * en caso de ser verdadero que el movimiento es horizontal
+     * @return horizontal
+     */
     public boolean esHorizontal() {
         boolean horizontal = false;
         if (posIni.getFila() == posFin.getFila() && posIni.getColumna() != posFin.getColumna()) {
@@ -63,6 +101,12 @@ public class Movimiento {
         }
         return horizontal;
     }
+
+    /**
+     * Método esDiagonal que no recibe parámetros de entrada y nos devuelve un booleano (diagonal) que nos indica
+     * en caso de ser verdadero que el movimiento es diagonal
+     * @return diagonal
+     */
     public boolean esDiagonal() {
         boolean diagonal = false;
         if (Math.abs(posIni.getColumna() - posFin.getColumna()) == Math.abs(posIni.getFila() - posFin.getFila())) {
@@ -70,6 +114,15 @@ public class Movimiento {
         }
         return diagonal;
     }
+
+    /**
+     * Método saltoHorizontal que recibe dos parámetros de entrada de tipo posición que corresponden
+     * a la posición inicial y final(posIni y posFin) y nos devuelve un entero (cant) que nos indica el número
+     * de saltos horizontales
+     * @param posIni Posición inicial
+     * @param posFin Posición final
+     * @return cant
+     */
     public int saltoHorizontal(Posicion posIni,Posicion posFin) {
         int cant = 0;
         if (esHorizontal()) {
@@ -77,6 +130,15 @@ public class Movimiento {
         }
         return cant;
     }
+
+    /**
+     * Método saltoVertical que recibe dos parámetros de entrada de tipo posición que corresponden
+     * a la posición inicial y final(posIni y posFin) y nos devuelve un entero (cant) que nos indica el número
+     * de saltos verticales
+     * @param posIni Posición inicial
+     * @param posFin Posición final
+     * @return cant
+     */
     public int saltoVertical(Posicion posIni,Posicion posFin) {
         int cant = 0;
         if (esVertical()) {
